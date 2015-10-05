f05457dcb80166a2dca51a128e8b35fb7afcbd4e
Andriy Bas <bass.andriy@gmail.com>
2015-10-05 23:27:10 +0300

    add utils class

---
+++ app/src/main/java/ua/com/dataforyou/utils/Utils.java
@@ -0,0 +1,11 @@
+package ua.com.dataforyou.utils;
+
+/**
+ * Created on 10/5/15.
+ */
+public class Utils {
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
