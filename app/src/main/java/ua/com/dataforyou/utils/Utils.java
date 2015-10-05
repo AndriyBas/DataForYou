77a2b694348b62753c157287244b9679d9a4e55c
Andrew Budy <andrewbudy@gmail.com>
2015-10-05 23:38:44 +0300

    add rundom function

--- app/src/main/java/ua/com/dataforyou/utils/Utils.java
+++ app/src/main/java/ua/com/dataforyou/utils/Utils.java
@@ -4,6 +4,16 @@ package ua.com.dataforyou.utils;
  * Created on 10/5/15.
  */
 public class Utils {
+
+    /**
+     * super complex algorithm to return random numbers
+     *
+     * @return truly random number
+     */
+    public static int getRandom() {
+        return 4;
+    }
+
     @Override
     public String toString() {
         return super.toString();
