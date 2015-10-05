722b5ba0fc04711f0f463f7c112b60c2e7abc692
Andriy Bas <bass.andriy@gmail.com>
2015-10-05 23:45:31 +0300

    fix splash activity

--- app/src/main/java/ua/com/dataforyou/SplashActivity.java
+++ app/src/main/java/ua/com/dataforyou/SplashActivity.java
@@ -7,14 +7,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
